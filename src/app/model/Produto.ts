40538a053397c777cd1a5caddafc8526a58b75c2
BartRamandu <bartmelo_93@hotmail.com>
2021-12-09 18:59:33 -0300

    @BartRamandu [Task: Created Models e Service and Modified tsconfig, app.module and components cadastrar]

---
+++ src/app/model/Produto.ts
@@ -0,0 +1,13 @@
+import { Categoria } from "./Categoria"
+import { Usuario } from "./Usuario"
+
+
+export class Produto{
+  public id: number
+  public produto: string
+  public valor: number
+  public estoque: number
+  public pesoUnitario: number
+  public categoriaDoProduto: Categoria
+  public criador: Usuario
+}
