9e7332ac9f9e5b33e4e5f027d8d55ebfc978fed0
BartRamandu <bartmelo_93@hotmail.com>
2021-12-15 12:16:36 -0300

    @BartRamandu [Task: Created Service produto and add paths in app-routing]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { CategoriaDeleteComponent } from './delete/categoria-delete/categoria-delete.component';
+import { CategoriaEditComponent } from './edit/categoria-edit/categoria-edit.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { InicioComponent } from './inicio/inicio.component';
 
@@ -8,9 +10,14 @@ const routes: Routes = [
 
   {path:"", redirectTo:"entrar", pathMatch:"full"},
 
-  {path:"entrar",component:EntrarComponent},
-  {path:"cadastrar",component:CadastrarComponent},
-  {path:"inicio",component:InicioComponent}
+  {path: "entrar", component: EntrarComponent},
+  {path: "cadastrar", component: CadastrarComponent},
+  {path: "inicio", component: InicioComponent},
+
+  {path: "categoria-edit", component: CategoriaEditComponent},
+  {path: "categoria-delete", component: CategoriaDeleteComponent},
+  {},
+  {},
 ];
 
 @NgModule({
